271dc6a5786d71e24921396e33d1eda094c1aae4
Rainer M Krug <Rainer@krugs.de>
2022-12-07 13:13:37 +0100

    Create redo_batch.sh

---
+++ bin/redo_batch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+$($(dirname "$0")/clean BATCH
+
+$($(dirname "$0")/do_all 20221107 BATCH
+$($(dirname "$0")/do_all 20221109 BATCH
+$($(dirname "$0")/do_all 20221111 BATCH
+$($(dirname "$0")/archive
+
+$($(dirname "$0")/do_all 20221114 BATCH
+$($(dirname "$0")/do_all 20221116 BATCH
+$($(dirname "$0")/do_all 20221118 BATCH
+$($(dirname "$0")/archive
+
+$($(dirname "$0")/do_all 20221121 BATCH
+$($(dirname "$0")/do_all 20221123 BATCH
+$($(dirname "$0")/do_all 20221125 BATCH
+$($(dirname "$0")/archive
+
+$($(dirname "$0")/do_all 20221128 BATCH
+$($(dirname "$0")/do_all 20221130 BATCH
+$($(dirname "$0")/do_all 20221202 BATCH
+$($(dirname "$0")/archive
+
+$($(dirname "$0")/do_all 20221205 BATCH
+$($(dirname "$0")/do_all 20221207 BATCH
+$($(dirname "$0")/archive
+
